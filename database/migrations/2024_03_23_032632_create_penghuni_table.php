c528b746b7a8b5ef01aca8dd20a48ef51bf87d1f
naufalpratam4 <naufal.d.pratama@gmail.com>
2024-03-23 10:55:02 +0700

    add migration

--- database/migrations/2024_03_23_032632_create_penghuni_table.php
+++ database/migrations/2024_03_23_032632_create_penghuni_table.php
@@ -17,14 +17,15 @@ return new class extends Migration
             $table->id();
             $table->string('nama_penghuni')->nullable();
             $table->string('email')->nullable();
-            $table->bigInteger('alamat')->nullable();
             $table->bigInteger('no_hp')->nullable();
             $table->date('tgl_masuk')->nullable();
             $table->date('tgl_keluar')->nullable();
-            $table->bigInteger('kamar')->nullable();
-            $table->bigInteger('pembayaran');
-            $table->foreign('kamar')->references('id')->on('kamar')->nullable();
-            $table->foreign('alamat')->references('id')->on('alamat')->nullable();
+            $table->unsignedBigInteger('alamat_jalan');
+            $table->unsignedBigInteger('kamar');
+            $table->unsignedBigInteger('pembayaran');
+            $table->foreign('alamat_jalan')->references('id')->on('alamat');
+            $table->foreign('kamar')->references('id')->on('kamar');
+            $table->foreign('pembayaran')->references('id')->on('pembayaran');
         });
 
         Schema::enableForeignKeyConstraints();
